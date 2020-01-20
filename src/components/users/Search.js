f3f5c2778e16c6129a3a0aefe24668fe4f718a8b
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-01-20 10:28:10 +0100

    Section 4:19 Done!
    - Google Chrome had a complaint about My cookies,
    'SameSite' seams to fix the problem.
    - Registerd to Use Github Api
    - Search functionality added. With moving state up the stream.
    - Clear button added. With moving state up the stream.
    - Destructuring.

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,28 +1,38 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
 export class Search extends Component {
 
     state = {
         text: ''
     };
 
-    //If we dont want to use arrow function we need to do a bind(this) for it to work. look on form
-    onSubmit(e) {
+    static propsTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+    }
+
+
+    onSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.text)
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: '' });
     }
 
     onChange = (event) => {
-        //Note
         this.setState({ [event.target.name]: event.target.value });
     }
     render() {
+        const { showClear, clearUsers } = this.props
+
         return (
             <div>
-                <form onSubmit={this.onSubmit.bind(this)} className="Form">
-                    <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange} />
-                    <input type="Submit" value="Search" className="btn btn-dark btn-block" />
+                <form onSubmit={this.onSubmit} className="Form">
+                    <input type="text" name="text" placeholder="Search Users..." onChange={this.onChange} value={this.state.text} />
+                    <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                {showClear && <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>}
+
             </div>
         )
     }
